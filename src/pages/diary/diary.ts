a546f7c5265e62ebd8e83bdc3e5865b35a7df3e5
chunhao <chunhao.liu@kgi.com>
2016-12-26 20:07:00 +0800

    A Rough implementation of 'DiaryPage'. There's css issue remaining ...

--- src/pages/diary/diary.ts
+++ src/pages/diary/diary.ts
@@ -1,11 +1,26 @@
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
 
+export class Diary {
+  id: number;
+  name: string;
+  title: string;
+  date: string;
+  content: string;
+}
+
 @Component({
   selector: "diary-page",
   templateUrl: "diary.html"
 })
 export class DiaryPage {
-
+  title = 'Details of My Diary';
+  diary: Diary = {
+    id: 1,
+    name: 'Nobody',
+    title: 'One Day In Mt. Fuji',
+    date : '2016-12-26',
+    content: 'La La Land !'
+  };
   constructor(public navCtrl: NavController) {}
 }
