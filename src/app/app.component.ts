c27aa8e46ebce4a18d670aac4c56a90b5cbcfffc
chunhao <chunhao.liu@kgi.com>
2016-12-28 14:17:16 +0800

    To store/restore diary content, data persistence operation added in 'Diary Page' & 'Entry Page' using SQLite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,56 @@ import { Component } from "@angular/core";
 import { Platform } from "ionic-angular";
 import { StatusBar, Splashscreen } from "ionic-native";
 import { HomePage } from "../pages/home/home";
+import { SQLite } from 'ionic-native';
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
   rootPage = HomePage;
+  db : SQLite;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
+
+    // let db = new SQLite();
+      this.db = new SQLite();
+      this.db.openDatabase({
+          name: "MyDiary.db",
+          location: "default"
+      }).then(() => {
+          this.create();
+
+          this.add("Nobody", "Eat"  ,"2016-01-01","Hamberger x 1");
+          this.add("Nobody", "Sleep","2016-01-02","12 hours");
+          this.add("Nobody", "Play" ,"2016-01-03","2 hours of video game");
+          this.add("Nobody", "Study","2016-01-04","2 hours of reading");
+      }, (error) => {
+          console.error("Unable to open database", error);
+      });
+
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  public create(){
+  //  db.executeSql("CREATE TABLE IF NOT EXISTS dairy (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, title TEXT, date TEXT, content TEXT)", {})
+      this.db.executeSql("CREATE TABLE IF NOT EXISTS dairy (name TEXT, title TEXT, date TEXT PRIMARY KEY, content TEXT)", {})
+        .then((data) => {
+          console.log("TABLE CREATED: ", data);
+      }, (error) => {
+          console.error("Unable to execute sql", error);
+      })
+  }
+
+  public add(name:string, title:string, date:string, content:string) {
+      this.db.executeSql("INSERT INTO diary (name, title, date, content) VALUES (?,?,?,?,?)", [name, title, date, content])
+          .then((data) => {
+              console.log("INSERTED: " + JSON.stringify(data));
+      }, (error) => {
+          console.log("ERROR: " + JSON.stringify(error.err));
+      });
   }
 }
