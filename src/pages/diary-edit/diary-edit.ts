904de705af2c740be8ad1b4c45160ea0aa9cb8ca
Kuolung <KuolungCheng@users.noreply.github.com>
2017-01-18 01:13:29 +0800

    Feat: Update DiaryEditPage

--- src/pages/diary-edit/diary-edit.ts
+++ src/pages/diary-edit/diary-edit.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DatePicker } from 'ionic-native';
 import * as moment from 'moment';
 
 export interface Diary {
@@ -15,16 +16,28 @@ export interface Diary {
   templateUrl: 'diary-edit.html'
 })
 export class DiaryEditPage {
+  now: any;
   diaryMonth: any;
-  diaryWeek: any;
   diaryDay: any;
+  diaryDate: any;
   diaryTime: any;
 
   constructor(private navController: NavController) {
     moment.locale('zh-tw');
-    this.diaryMonth = moment().format('MMMM');
-    this.diaryWeek = moment().format('dddd');
-    this.diaryDay = moment().format('D');
-    this.diaryTime = moment().format('HH:mm');
+    this.now = moment().format('YYYYMMDDHHmm');
+    this.diaryMonth = moment(this.now, 'YYYYMMDDHHmm').format('MMMM');
+    this.diaryDay = moment(this.now, 'YYYYMMDDHHmm').format('dddd');
+    this.diaryDate = moment(this.now, 'YYYYMMDDHHmm').format('D');
+    this.diaryTime = moment(this.now, 'YYYYMMDDHHmm').format('HH:mm');
+  }
+
+  openDatePicker() {
+    DatePicker.show({
+      date: this.now,
+      mode: 'datetime'
+    }).then(
+      date => console.log('Got date: ', date),
+      err => console.log('Error occurred while getting date: ', err)
+    );
   }
 }
